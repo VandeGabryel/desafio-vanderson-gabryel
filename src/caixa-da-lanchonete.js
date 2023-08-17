0e01384b930f162c9c8050c8deb0f79097c2d797
Vanderson Gabryel <139379635+VandeGabryel@users.noreply.github.com>
2023-08-16 22:05:30 -0300

    Add files via upload

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,40 @@
+class CaixaDaLanchonete {
+
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        return "";
+    }
+
+}
+
+function  insert(num){
+var itens = document.getElementById('resultado').innerHTML;
+document.getElementById('resultado').innerHTML = itens + num;
+}
+
+function soma (resultado , itens){
+var cafe = document.getElementById("cafe").value;
+var chantily = document.getElementById("chantily").value;
+var suco = document.getElementById("suco").value;
+var sanduiche = document.getElementById("sanduiche").value;
+var queijo = document.getElementById("queijo").value;
+var salgado = document.getElementById("salgado").value;
+var combo1 = document.getElementById("combo1").value;
+var combo2 = document.getElementById("combo2").value;
+var resultado = document.getElementById("resultado");
+cafe= parseFloat (3.00);
+chantily= parseFloat (1.50);
+suco= parseFloat (6.20);
+sanduiche= parseFloat (6.50);
+queijo= parseFloat (2.00);
+salgado= parseFloat (7.25);
+combo1= parseFloat (9.50);
+combo2= parseFloat (7.50);
+resultado.innerHTML= id+id
+
+
+    // document.getElementById('resultado').innerHTML = parseFloat(somaItens) + parseFloat(num);
+    // return insert + insert
+}
+
+
+// export { CaixaDaLanchonete };
